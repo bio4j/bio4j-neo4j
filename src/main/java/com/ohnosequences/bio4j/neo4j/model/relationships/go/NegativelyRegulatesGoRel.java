e6a33597e1448c2b8c9eaa40c69c1db7c6554ebb
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2013-12-03 11:22:35 +0100

    refactoring packages adapting them to new prefix ohnosequences

---
+++ src/main/java/com/ohnosequences/bio4j/neo4j/model/relationships/go/NegativelyRegulatesGoRel.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.ohnosequences.bio4j.neo4j.model.relationships.go;
+
+import com.ohnosequences.neo4j.BasicRelationship;
+import org.neo4j.graphdb.Relationship;
+
+/**
+ *
+ * GO term negatively regulates relationship
+ *
+ * @author Pablo Pareja Tobes <ppareja@era7.com>
+ */
+public class NegativelyRegulatesGoRel extends BasicRelationship{
+
+    public static final String NAME = "NEGATIVELY_REGULATES_GO";
+
+    public static final String OBOXML_RELATIONSHIP_NAME = "negatively_regulates";
+
+    public NegativelyRegulatesGoRel(Relationship rel){
+        super(rel);
+    }
+
+    @Override
+    public String name() {
+        return NAME;
+    }
+
+}
