8b2b05c77371c0aaf0e642456031f06e51d9cc08
Pablo Pareja Tobes <pabloparejatobes@gmail.com>
2014-07-08 10:51:31 +0200

    starting with go implementation

--- src/main/java/com/bio4j/neo4j/model/go/Neo4jGoGraph.java
+++ src/main/java/com/bio4j/neo4j/model/go/Neo4jGoGraph.java
@@ -1,9 +1,27 @@
 package com.bio4j.neo4j.model.go;
 
+import com.bio4j.model.go.GoGraph;
+import com.ohnosequences.typedGraphs.neo4j.Neo4jTypedGraph;
+import org.neo4j.graphdb.GraphDatabaseService;
+
 /**
  * Created by ppareja on 7/3/2014.
  */
 public class Neo4jGoGraph implements
 		Neo4jTypedGraph,
 		GoGraph{
+
+	protected GraphDatabaseService rawGraph;
+
+	Neo4jGoGraph(GraphDatabaseService rawGraph) {
+
+		this.rawGraph = rawGraph;
+	}
+
+	@Override
+	public GraphDatabaseService rawGraph() {
+		return rawGraph;
+	}
+
+
 }
